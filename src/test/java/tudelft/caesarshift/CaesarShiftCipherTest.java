7635efb4dc4e178138123d737ca4c2e73b441863
VictoriaAS <victoriaasnina@gmail.com>
2020-12-18 15:39:03 -0500

    update1

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,39 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class CaesarShiftCipherTest {
+    @Test
+    public void Shift3() {
+        CaesarShiftCipher shif = new CaesarShiftCipher();
+        String s =
+                shif.CaesarShiftCipher("abc", 3);
+        Assertions.assertEquals("def", s);
+    }
+
+    @Test
+
+    public void InvalidChar() {
+        CaesarShiftCipher shif = new CaesarShiftCipher();
+        String s =
+                shif.CaesarShiftCipher("r45", 3);
+        Assertions.assertEquals("invalid", s);
+    }
+    @Test
+
+    public void ShiftMinus3() {
+        CaesarShiftCipher shif = new CaesarShiftCipher();
+        String s =
+                shif.CaesarShiftCipher("def", -3);
+        Assertions.assertEquals("abc", s);
 }
+    @Test
+
+    public void ShiftUpperChar() {
+        CaesarShiftCipher shif = new CaesarShiftCipher();
+        String s =
+                shif.CaesarShiftCipher("ABC", -3);
+        Assertions.assertEquals("invalid", s);
+
+    }}
